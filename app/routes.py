a6dfceb418c59c1c80daa504594f34bd211b36eb
Egor Lugovkin <mrpostik@gmail.com>
2022-02-13 18:21:09 +0700

    add directory for public site page. Update .gitignore

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
+
 from app import app
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return "Hello world"
+    return render_template('public.html')
