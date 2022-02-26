246b8b90653cc40e47c0f1a80c464dc06d0b8682
Egor Lugovkin <mrpostik@gmail.com>
2022-02-26 14:23:49 +0700

    Добавил страницу регистрации

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,6 @@ from app import app
 def index():
     return render_template('public/public.html')
 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    return render_template('register/register.html')
