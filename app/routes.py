94d7c8f9173f39ab88064c72f8785e3441812a31
Alexey Mikhailov <mikhailovalexey04.02.2003@gmail.com>
2022-03-04 18:07:15 +0700

    Добавлена страница страница аккаунта Добавлен стиль css к странице аккаунта

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,12 @@ from app import app
 def index():
     return render_template('public/public.html')
 
+
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     return render_template('register/register.html')
+
+
+@app.route('/account', methods=['POST', 'GET'])
+def account():
+    return render_template('account/account.html')
